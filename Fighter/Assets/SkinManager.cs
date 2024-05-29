6a051209ff4ceb6b535d8d33f50f21e4235944b4
ArcCreate <arckumar12@gmail.com>
2024-05-28 19:52:23 -0700

    Added other character spritesheets

--- Fighter/Assets/SkinManager.cs
+++ Fighter/Assets/SkinManager.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class SkinManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    //refrences
+    public SpriteRenderer spriteRenderer;
+    public List<Sprite> skins = new List<Sprite>();
+    private int selectedSkin = 0;
+
+    public void Next()
     {
-        
+        selectedSkin = selectedSkin + 1;
+        if( selectedSkin == skins.Count)
+        {
+            selectedSkin = 0;
+        }
+        spriteRenderer.sprite = skins[selectedSkin];
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Back()
     {
-        
+        Debug.Log("back");
+        selectedSkin = selectedSkin - 1;
+        if (selectedSkin < 0)
+        {
+            selectedSkin = skins.Count - 1;
+        }
+        spriteRenderer.sprite = skins[selectedSkin];
+    }
+
+    public void Play()
+    {
+        PlayerPrefs.SetInt("player1Selection", selectedSkin);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
