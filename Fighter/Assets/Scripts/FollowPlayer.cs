856e8ade2283ff8197d4fe92844c1d1c57c054c3
ArcCreate <arckumar12@gmail.com>
2024-05-31 09:20:14 -0700

    Fixed dash slider bug for player 1

--- Fighter/Assets/Scripts/FollowPlayer.cs
+++ Fighter/Assets/Scripts/FollowPlayer.cs
@@ -10,17 +10,25 @@ public class FollowPlayer : MonoBehaviour
 
     void Start()
     {
-        if(PlayerNumber == 1)
+        StartCoroutine(CheckForTargetScript());
+    }
+
+    private IEnumerator CheckForTargetScript()
+    {
+        // Wait for a bit before checking for the targetScript
+        yield return new WaitForSeconds(1.0f); // Adjust the wait time as needed
+
+        if (PlayerNumber == 1)
         {
             // Find the first object with the TargetScript attached
             PlayerController targetScript = FindObjectOfType<PlayerController>();
             if (targetScript != null)
             {
+                Debug.Log("targetScript is there");
                 player = targetScript.gameObject.transform;
             }
-
         }
-        else if(PlayerNumber == 2)
+        else if (PlayerNumber == 2)
         {
             // Find the first object with the TargetScript attached
             PlayerController2 targetScript = FindObjectOfType<PlayerController2>();
